46feb167cf4501d0b04a416d10c2d19900434eed
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-18 19:27:17 +0200

    add: added method to create the two times table up to the input

--- snack-7/js/script-snack.js
+++ snack-7/js/script-snack.js
@@ -1,6 +1,24 @@
 "use strict";
 
-//CICLO TABELLINA DEL DUE FINO AL NUMERO 1000
-for (let i = 0; i < 501; i++) {
-    console.log("VALORE ---> " + (2 * i));
+//VARIABILE INPUT
+const input = prompt("scegli il numero massimo consentito");
+
+/*
+  -CICLO TABELLINA DEL DUE FINO AL NUMERO CONSENTITO;
+  -VIENE CONTROLLATO SE L'INPUT E' NEGATIVO O POSITIVO, QUINDI STAMPA LA TABELLINA
+   NEGATIVA O POSITIVIA;
+  -VIENE CONTROLLATO SE L'INPUT E' UN INTERO E UN NUMERO, NEL CASO CONTRARIO VIENE
+   RICARICATA LA PAGINA HTML;
+*/
+if (!isNaN(input) && parseInt(input) < 0) {
+    for (let i = 0; i > parseInt(input) - 1; i--) {
+        console.log("VALORE ---> " + (2 * i));
+    }
 }
+else if (!isNaN(input)) {
+    for (let i = 0; i < parseInt(input) + 1; i++) {
+        console.log("VALORE ---> " + (2 * i));
+    }
+}
+else
+    location.reload();
